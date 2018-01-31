f0a6bd6cc3364aeeeeb920508d2b4afb14284451
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-31 15:15:31 -0500

    up

--- AngularCoreProto/ClientApp/app/app.module.ts
+++ AngularCoreProto/ClientApp/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
